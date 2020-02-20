0407b947b0f2f4e438f454a34606c7c883394f3b
jiangh10086 <1174780245@qq.com>
2020-02-20 15:42:47 +0800

    Update openinstall-for-ios.js

--- openinstall-for-ios.js
+++ openinstall-for-ios.js
@@ -3,8 +3,7 @@ var userAgent = navigator.userAgent;
 var isIosClient = !!userAgent.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/);
 var openinstallForIosIsReady = false;
 var openinstallForIos = null;
-if (isIosClient){
-    alert(1);
+if (isIosClient){ 
     var urldata = OpenInstall.parseUrlParams();
     openinstallForIos = new OpenInstall({ 
         appKey : iosOpeninstallKey, 
