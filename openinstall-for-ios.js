dbc206f7242a7ad0e831c8226e048d860e8a854e
jiangh10086 <1174780245@qq.com>
2020-02-20 15:53:26 +0800

    Update openinstall-for-ios.js

--- openinstall-for-ios.js
+++ openinstall-for-ios.js
@@ -3,12 +3,21 @@ var userAgent = navigator.userAgent;
 var isIosClient = !!userAgent.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/);
 var openinstallForIosIsReady = false;
 var openinstallForIos = null;
-if (isIosClient){ 
+if (isIosClient){
     var urldata = OpenInstall.parseUrlParams();
     openinstallForIos = new OpenInstall({ 
         appKey : iosOpeninstallKey, 
         onready : function() { 
-            m.schemeWakeup(); 
+            openinstallForIosIsReady = true;
+            openinstallForIos.schemeWakeup(); 
         }
     }, urldata);
+}
+
+function DownLoadIosApp() {
+    if (openinstallForIosIsReady) {
+        openinstallForIos.wakeupOrInstall();
+    }else{
+        window.location.href="https://apps.apple.com/cn/app/the-curvy-line/id1494555237"
+    }
 }
